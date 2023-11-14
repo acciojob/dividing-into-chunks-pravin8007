42cb99296f3cdf776d3de53dc7549c7040e9ad86
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-14 13:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const arr = [1, 2, 3, 4, 1, 2, 2];
+const arr = [1, 2, 3, 4, 1,0,2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-	 let result = [];
+	
+	let result = [];
     let currentSubarray = [];
-
     let currentSum = 0;
+	
 
-    for (let num of arr) {
-        if (currentSum + num <= n) {
-            currentSubarray.push(num);
-            currentSum += num;
-        } else {
+    for (let num = 0 ; num < arr.length ; num++) {
+        if (currentSum + arr[num] < n) {
+            currentSubarray.push( arr[num]);
+            currentSum += arr[num];
+        } 
+		else if(currentSum +  arr[num] == n) {
+			currentSubarray.push(arr[num]);
+			currentSum += arr[num];
+			num++;
+		}
+		else {
             result.push(currentSubarray);
-            currentSubarray = [num];
-            currentSum = num;
+            currentSubarray = [arr[num]];
+            currentSum = arr[num];
         }
     }
 
